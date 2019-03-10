5e7289b433d61fedb620782a37edeafe23cc9d38
Matt <warrenma@uk.ibm.com>
2019-03-10 18:16:19 +0000

    change print to echo

--- unixsetup.sh
+++ unixsetup.sh
@@ -1,32 +1,32 @@
-print
-print "Creating dir strutures..."
+echo
+echo "Creating dir strutures..."
 if [ ! -d /opt ];
 then
 	mkdir /opt
 else
-	print "/opt found."
+	echo "/opt found."
 fi
 if [ ! -d /opt/yab ];
 then
 	mkdir /opt/yab
 else
-	print "/opt/yab found."
+	echo "/opt/yab found."
 fi
 if [ ! -d /opt/yab/FatController ];
 then
 	mkdir /opt/yab/FatController
 else
-	print "/opt/yab/FatController found."
-	print "This install will overwrite existing files."
+	echo "/opt/yab/FatController found."
+	echo "This install will overwrite existing files."
 fi
 if [ ! -d /opt/yab/FatController/data ];
 then
 	mkdir /opt/yab/FatController/data
 else
-	print "/opt/yab/FatController/data found. I will leave your data files intact."
+	echo "/opt/yab/FatController/data found. I will leave your data files intact."
 fi
-print
-print "Copying files..."
+echo
+echo "Copying files..."
 for file in $(ls)
 do
 	cp "${file}" /opt/yab/FatController
